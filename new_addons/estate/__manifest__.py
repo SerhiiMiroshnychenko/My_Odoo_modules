be5c555eab223caf17a09547f824308a976d4e23
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-07-05 17:22:41 +0300

    Add estate module with __manifest__.py

---
+++ new_addons/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Real estate advertising module',
+    'description': """
+    A simple real estate advertising module was created for educational purposes
+    """,
+    'depends': ['base'],
+    'author': 'Serhii Miroshnychenko',
+    'license': 'LGPL-3',
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
