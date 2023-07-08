6567f2da24e6713c3a23cfced4fe9619f0e05ddb
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-07-08 13:48:26 +0300

    Chapter 6: Finally, Some UI To Play With

--- new_addons/estate/__manifest__.py
+++ new_addons/estate/__manifest__.py
@@ -10,6 +10,8 @@
     'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
--- new_addons/estate/models/estate_property.py
+++ new_addons/estate/models/estate_property.py
@@ -4,13 +4,17 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "A model representing real estate objects"
 
-    name = fields.Char(required=True)
+    title = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,
+                                    default=lambda self: fields.Datetime.add(
+        value=fields.Datetime.today(),
+        months=3
+    ))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,5 +23,20 @@ class EstateProperty(models.Model):
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="The side of the world on which the garden is oriented")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        string='State',
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new',
+        help="The state of the real estate object"
+    )
 
 
